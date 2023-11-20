a7bd49fe4c6e01cac71e0df97d5af6b08b4b6648
QUOCDUNG\dungnghiem <dungnghiem0203@gmail.com>
2023-11-20 23:41:01 +0700

    second

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'resdb',
-    connectionLimit: 10
+    host: process.env.Db_host,
+    user: process.env.Db_user,
+    password: process.env.Db_password,
+    database: process.env.Db_database,
+    connectionLimit: process.env.Db_connectionLimit
 });
 
 module.exports = pool
