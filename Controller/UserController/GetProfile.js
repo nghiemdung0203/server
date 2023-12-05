f7c27294a56bc39cc5a1827098de4907e958f7c2
QUOCDUNG\dungnghiem <dungnghiem0203@gmail.com>
2023-12-05 21:42:14 +0700

    fix getProfiles

--- Controller/UserController/GetProfile.js
+++ Controller/UserController/GetProfile.js
@@ -4,7 +4,7 @@ module.exports.GetProfiles = (req, res) => {
     const UserID = req.query.UserID;
     console.log(UserID)
     pool.query(
-        'Select profile.ID, profile.UserID, profile.Role, users.PhoneNumber ,restaurant.Name, restaurant.Address, restaurant.PhoneNumber from profile INNER JOIN restaurant ON profile.RestaurantID = restaurant.ID INNER JOIN users ON users.ID = profile.UserID Where UserID = ?',
+        'Select profile.ID, profile.UserID, profile.Role, users.PhoneNumber ,restaurant.Name, restaurant.Address, restaurant.PhoneNumber from profile INNER JOIN restaurant ON profile.RestaurantID = restaurant.ID INNER JOIN users ON users.uid = profile.UserID Where UserID = ?',
         [UserID],
         (error, result) => {
             if (error) {
