b071c1395194df8592535643e7d9d738357dd1c7
QUOCDUNG\dungnghiem <dungnghiem0203@gmail.com>
2023-12-14 15:51:55 +0700

    cr

--- Controller/RestaurantController/CreateRestaurant.js
+++ Controller/RestaurantController/CreateRestaurant.js
@@ -3,6 +3,8 @@ const pool = require("../../database");
 module.exports.CreateRestaurant = async(req, res) => {
   const { name, address, phoneNumber, description, Time, Number_of_table } = req.body;
 
+  const Number_of_Tables = parseInt(Number_of_table, 10)
+
   const AvatarPath = req.file.path
     const Avatar = await cloudinary.uploader.upload(AvatarPath, {
       resource_type: "image",
@@ -11,12 +13,12 @@ module.exports.CreateRestaurant = async(req, res) => {
 
   pool.query(
     "INSERT INTO restaurant (Name, Address, PhoneNumber, Avatar, Number_of_tables, Time, Description) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    [name, address, phoneNumber, Avatar, description, Time, Number_of_table],
+    [name, address, phoneNumber, Avatar, description, Time, Number_of_Tables],
     (error, result) => {
         if (error) {
             res.status(500).send(error)
         } else {
-            for (let i = 0; i < Number_of_table; i++) {
+            for (let i = 0; i < Number_of_Tables; i++) {
               pool.query('INSERT INTO tableForRestaurant (NumberOfcustomer, RestaurantID) VALUES (?, ?)', [0, result.insertId], (err, ress) => {
                 if (err) {
                   return res.status(500).send(err);
