078d1b0160e7fc8f65c86445ae46303e71b3fc2d
QUOCDUNG\dungnghiem <dungnghiem0203@gmail.com>
2023-12-18 20:37:34 +0700

    menu

--- Controller/RestaurantController/DeleteEmeployeeToRestaurant.js
+++ Controller/RestaurantController/DeleteEmeployeeToRestaurant.js
@@ -3,7 +3,7 @@ const pool = require('../../database');
 module.exports.GetMenu = (req, res) => {
     const {profileID, Restaurant_id} = req.body
     pool.query(
-        'Select Menu.Menu_id, restaurant.Name, restaurant.Address, restaurant.PhoneNumber, MenuItems.FoodID, MenuItems.DrinkID, food.Name, food.DescribeFood, food.Food_Price, food.Avatar, drinks.Drink_name, drinks.Drink_size, drinks.Drink_Description from Menu INNER JOIN MenuItems ON Menu.Menu_id = MenuItems.Menu_id LEFT JOIN food ON MenuItems.Food_id = food.Food_id LEFT JOIN drinks ON MenuItems.Drink_id = drinks.Drink_id where Menu.Restaurant_id = ?',[Restaurant_id],
+        'Update profile '
         (error, result) => {
             if (error) {
                 return res.status(500).send(error);
