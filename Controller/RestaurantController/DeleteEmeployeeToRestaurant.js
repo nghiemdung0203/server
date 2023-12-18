ba0f22ec76350ca5d3be647baca49d8fe393365e
QUOCDUNG\dungnghiem <dungnghiem0203@gmail.com>
2023-12-18 21:52:32 +0700

    menu

--- Controller/RestaurantController/DeleteEmeployeeToRestaurant.js
+++ Controller/RestaurantController/DeleteEmeployeeToRestaurant.js
@@ -2,14 +2,20 @@ const pool = require("../../database");
 
 module.exports.DeleteEmeployeeToRestaurant = (req, res) => {
   const { profileID, Restaurant_id } = req.body;
-  pool.query('Select RestaurantID from profile where ID = ?', [profileID], (error, result) => {
-    if (error) {
-        res.status(500).send(error)
-    } else {
+  pool.query(
+    "Select RestaurantID from profile where ID = ?",
+    [profileID],
+    (error, result) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        console.log(result)
+        console.log(typeof(result))
+        console.log(typeof(Restaurant_id))
         if (result !== Restaurant_id) {
-            res.status(400).send('Profile not found')
-        } 
-        pool.query(
+          return res.status(400).send("Profile not found");
+        } else {
+          pool.query(
             "Update profile Set RestaurantID = NULL, isWorking = 0, CodeName = NULL, Time = NULL WHERE ID = ?",
             [profileID],
             (error, result) => {
@@ -20,6 +26,8 @@ module.exports.DeleteEmeployeeToRestaurant = (req, res) => {
               }
             }
           );
+        }
+      }
     }
-  })
+  );
 };
