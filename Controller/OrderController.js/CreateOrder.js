9c12cb1bf8093f8aab7f8848c5168d952d11790d
Nghiêm Dũng <73541931+nghiemdung0203@users.noreply.github.com>
2023-12-19 14:51:20 +0000

    s

--- Controller/OrderController.js/CreateOrder.js
+++ Controller/OrderController.js/CreateOrder.js
@@ -2,10 +2,10 @@ const pool = require("../../database");
 
 module.exports.CreateOrder = (req, res) => {
   const { profileID } = req.user;
-  const { Table_Number, Waitress_id, Restaurant_id, NumberOfCutomers, OrderDate } = req.body;
+  const { Table_Number, Waitress_id, Restaurant_id, NumberOfCutomers, OrderDate, Note } = req.body;
   pool.query(
-    "INSERT INTO orders (Customer_id, Table_Number, Waitress_id, Restaurant_id, NumberOfCustomers, OrderDate) VALUES (?, ?, ?, ?, ?, ?)",
-    [profileID, null, null, Restaurant_id, NumberOfCutomers, OrderDate],
+    "INSERT INTO orders (Customer_id, Table_Number, Waitress_id, Restaurant_id, NumberOfCustomers, OrderDate, Note) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    [profileID, null, null, Restaurant_id, NumberOfCutomers, OrderDate, Note],
     (error, result) => {
       if (error) {
         return res.status(500).send(error);
