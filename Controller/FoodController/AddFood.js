a41a49342382d0ceca51d53cdf99d7774ed720e1
QUOCDUNG\dungnghiem <dungnghiem0203@gmail.com>
2023-12-19 10:49:16 +0700

    Orders

--- Controller/FoodController/AddFood.js
+++ Controller/FoodController/AddFood.js
@@ -2,22 +2,33 @@ const cloudinary = require("../../cloudinary");
 const pool = require("../../database");
 
 module.exports.AddFood = async (req, res) => {
-  const { Name, DescribeFood, Food_Price } = req.body;
-  const AvatarPath = req.file.path
+  const { Name, DescribeFood, Food_Price, Menu_id } = req.body;
+  const AvatarPath = req.file.path;
   const Avatar = await cloudinary.uploader.upload(AvatarPath, {
     resource_type: "image",
-    folder: 'Food'
+    folder: "Food",
   });
-  pool.query("INSERT INTO food (Name, DescribeFood, Food_Price, Avatar) VALUES (?, ?, ?, ?)", [Name, DescribeFood, Food_Price, Avatar.secure_url], (error, result) => {
-    if (error) {
-      return res.status(400).send({error});
-    } else {
+  pool.query(
+    "INSERT INTO food (Name, DescribeFood, Food_Price, Avatar) VALUES (?, ?, ?, ?)",
+    [Name, DescribeFood, Food_Price, Avatar.secure_url],
+    (error, result) => {
+      if (error) {
+        return res.status(400).send({ error });
+      } else {
+        // Add món này vào menu luôn
 
-      // Add món này vào menu luôn
-
-      
-      return res.status(200).send("Add food successfully");
+        pool.query(
+          "INSERT INTO MenuItems (Menu_id, Food_id) VALUES (?, ?)",
+          [Menu_id, result.insertId],
+          (err, ress) => {
+            if (err) {
+              return res.status(400).send(error);
+            } else {
+              return res.status(200).send("Add food successfully");
+            }
+          }
+        );
+      }
     }
-  })
-
+  );
 };
--- Controller/RestaurantController/GetRestaurant.js
+++ Controller/RestaurantController/GetRestaurant.js
@@ -9,7 +9,7 @@ module.exports.getRestaurant = (req, res) => {
       return res.status(500).send(error);
     } else {
         pool.query(
-          "Select Menu_id, profile.Role, restaurant.Name, restaurant.Address, restaurant.PhoneNumber, restaurant.Avatar, restaurant.Description, restaurant.Number_of_tables, restaurant.Time from profile INNER JOIN restaurant ON profile.RestaurantID = restaurant.ID Inner Join Menu ON restaurant.ID = Menu.Restaurant_id WHERE profile.ID = ?",
+          "Select profile.Role, restaurant.Name, restaurant.Address, restaurant.PhoneNumber, restaurant.Avatar, restaurant.Description, restaurant.Number_of_tables, restaurant.Time, Menu.Menu_id from profile INNER JOIN restaurant ON profile.RestaurantID = restaurant.ID INNER JOIN Menu ON Menu.Restaurant_id = restaurant.ID WHERE profile.ID = ?",
           [profileID],
           (error, result) => {
             if (error) {
