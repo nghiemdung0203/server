52208795c6469e5a68bf71bc55ebe8f9e37dc51e
Nghiêm Dũng <73541931+nghiemdung0203@users.noreply.github.com>
2023-12-22 08:16:10 +0000

    s

--- Controller/UserController/SignIn.js
+++ Controller/UserController/SignIn.js
@@ -1,13 +1,13 @@
-const bcrypt = require('bcrypt');
-const pool = require('../../database');
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const pool = require("../../database");
+const jwt = require("jsonwebtoken");
 
 module.exports.SignIn = (req, res) => {
   const { email, uid, token } = req.body;
 
   // Query the database to check if the user exists with the provided email
   pool.query(
-    "SELECT * FROM users WHERE Email = ?",
+    "SELECT * FROM users WHERE email = ?",
     [email],
     (error, results) => {
       if (error) {
@@ -21,15 +21,17 @@ module.exports.SignIn = (req, res) => {
 
       const user = results[0]; // Assuming the query returns only one user
 
-      pool.query('UPDATE users SET token = ? WHERE uid = ?', [token, uid], (err, ress) => {
-        if (err) {
-          return res.status(500).send("Invalid credentials");
-        } else {
-          return res.status(200).json(user)
+      pool.query(
+        "UPDATE users SET token=? WHERE uid=?",
+        [token, uid],
+        (err, ress) => {
+          if (err) {
+            return res.status(500).send(err);
+          } else {
+            return res.status(200).json(user);
+          }
         }
-      })
+      );
     }
   );
 };
-
-
