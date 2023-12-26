89bbbb853f6cc238dfcc99606eb221130e4a559a
Nghiêm Dũng <73541931+nghiemdung0203@users.noreply.github.com>
2023-12-26 10:06:29 +0000

    s

--- Controller/RestaurantController/RevenuInAWeek..js
+++ Controller/RestaurantController/RevenuInAWeek..js
@@ -1,58 +1,44 @@
 const pool = require("../../database");
 
-module.exports.RevenuInAWeek = (req, res) => {
+module.exports.RevenuInAWeek = async (req, res) => {
     const date = req.query.date;
     console.log(date);
     const revenue = [];
 
-    pool.query(
-        'SELECT DISTINCT DATE(STR_TO_DATE(orderDate, "%m/%d/%Y, %h:%i:%s %p")) AS orderDay FROM orders WHERE STR_TO_DATE(orderDate, "%m/%d/%Y, %h:%i:%s %p") BETWEEN DATE_SUB(STR_TO_DATE(?, "%m/%d/%Y, %h:%i:%s %p"), INTERVAL 7 DAY) AND STR_TO_DATE(?, "%m/%d/%Y, %h:%i:%s %p")',
-        [date, date],
-        (error, result) => {
-            if (error) {
-                return res.status(500).send(error);
-            }
+    try {
+        const result = await pool.query(
+            'SELECT DISTINCT DATE(STR_TO_DATE(orderDate, "%m/%d/%Y, %h:%i:%s %p")) AS orderDay FROM orders WHERE STR_TO_DATE(orderDate, "%m/%d/%Y, %h:%i:%s %p") BETWEEN DATE_SUB(STR_TO_DATE(?, "%m/%d/%Y, %h:%i:%s %p"), INTERVAL 7 DAY) AND STR_TO_DATE(?, "%m/%d/%Y, %h:%i:%s %p")',
+            [date, date]
+        );
 
-            // Use a Promise to ensure all inner queries are executed before sending the response
-            Promise.all(
-                result.map((dayRow) => {
-                    return new Promise((resolve) => {
-                        const day = dayRow.orderDay;
+        await Promise.all(
+            result.map(async (dayRow) => {
+                const day = dayRow.orderDay;
 
-                        pool.query(
-                            'SELECT * FROM orders WHERE DATE(STR_TO_DATE(orderDate, "%m/%d/%Y, %h:%i:%s %p")) = ?',
-                            [day],
-                            (err, dayOrders) => {
-                                if (err) {
-                                    resolve({ error: err.message });
-                                } else {
-                                    const dayRevenue = dayOrders.reduce(
-                                        (acc, order) => {
-                                            const orderItems = order.OrderItems || [];
-                                            const orderRevenue = orderItems.reduce(
-                                                (itemAcc, item) => itemAcc + item.Food_Price * item.Quantity,
-                                                0
-                                            );
-                                            return acc + orderRevenue;
-                                        },
-                                        0
-                                    );
+                const dayOrders = await pool.query(
+                    'SELECT * FROM orders WHERE DATE(STR_TO_DATE(orderDate, "%m/%d/%Y, %h:%i:%s %p")) = ?',
+                    [day]
+                );
 
-                                    revenue.push({ day, dayRevenue });
-                                    resolve(revenue);
-                                }
-                            }
+                const dayRevenue = dayOrders.reduce(
+                    (acc, order) => {
+                        const orderItems = order.OrderItems || [];
+                        const orderRevenue = orderItems.reduce(
+                            (itemAcc, item) => itemAcc + item.Food_Price * item.Quantity,
+                            0
                         );
-                    });
-                })
-            )
-                .then((responses) => {
-                    // After all inner queries are executed, send the final response
-                    return res.status(200).send(responses);
-                })
-                .catch((err) => {
-                    return res.status(500).send(err);
-                });
-        }
-    );
+                        return acc + orderRevenue;
+                    },
+                    0
+                );
+
+                revenue.push({ day, dayRevenue });
+            })
+        );
+
+        // After all inner queries are executed, send the final response
+        res.status(200).send(revenue);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 };
