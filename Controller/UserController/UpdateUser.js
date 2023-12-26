30f7fee9b4736c5d7579ef6c8cb256c42882df53
Nghiêm Dũng <73541931+nghiemdung0203@users.noreply.github.com>
2023-12-26 09:33:28 +0000

    s

---
+++ Controller/UserController/UpdateUser.js
@@ -0,0 +1,42 @@
+const pool = require("../../database");
+const cloudinary = require("../../cloudinary");
+module.exports.UpdateUser = async (req, res) => {
+    const { Name, uid } = req.body;
+    console.log(DrinkID)
+    let Avatar = null;
+    if (req.file) {
+        const AvatarPath = req.file.path;
+        Avatar = await cloudinary.uploader.upload(AvatarPath, {
+            resource_type: "image",
+            folder: "Drink",
+        });
+    }
+
+    // Check if at least one property is provided for update
+    if (!(Name)) {
+        return res
+            .status(400)
+            .json({ error: "At least one property is required for update." });
+    }
+
+    // Build the SET clause for the SQL query dynamically based on provided parameters
+    const setClause = [];
+    if (Name) setClause.push(`Name = '${Name}'`);
+   
+    if (Avatar) setClause.push(`Avatar = '${Avatar.url}'`);
+    
+
+    // Construct the SQL query
+    const updateQuery = `UPDATE users SET ${setClause.join(
+        ", "
+    )} WHERE uid = ${uid}`;
+
+    // Execute the update query
+    pool.query(updateQuery, (error, result) => {
+        if (error) {
+            return res.status(500).json(error);
+        } else {
+            return res.status(200).json({ message: "User updated successfully" });
+        }
+    });
+};
