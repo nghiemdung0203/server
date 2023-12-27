820c51220da3e319d95e0a7f65a9a96692094204
Azz0102 <nguyenduchuyhht2@gmail.com>
2023-12-27 15:05:20 +0700

    huge update

--- Controller/UserController/UpdateUser.js
+++ Controller/UserController/UpdateUser.js
@@ -2,18 +2,15 @@ const pool = require("../../database");
 const cloudinary = require("../../cloudinary");
 module.exports.UpdateUser = async (req, res) => {
     const { Name, uid } = req.body;
-    console.log(DrinkID)
-    let Avatar = null;
-    if (req.file) {
-        const AvatarPath = req.file.path;
-        Avatar = await cloudinary.uploader.upload(AvatarPath, {
-            resource_type: "image",
-            folder: "Drink",
-        });
-    }
+
+    const AvatarPath = req.file.path;
+    const Avatar = await cloudinary.uploader.upload(AvatarPath, {
+        resource_type: "image",
+        folder: "Drink",
+    });
 
     // Check if at least one property is provided for update
-    if (!(Name)) {
+    if (!Name) {
         return res
             .status(400)
             .json({ error: "At least one property is required for update." });
@@ -22,9 +19,8 @@ module.exports.UpdateUser = async (req, res) => {
     // Build the SET clause for the SQL query dynamically based on provided parameters
     const setClause = [];
     if (Name) setClause.push(`Name = '${Name}'`);
-   
+
     if (Avatar) setClause.push(`Avatar = '${Avatar.url}'`);
-    
 
     // Construct the SQL query
     const updateQuery = `UPDATE users SET ${setClause.join(
@@ -36,7 +32,9 @@ module.exports.UpdateUser = async (req, res) => {
         if (error) {
             return res.status(500).json(error);
         } else {
-            return res.status(200).json({ message: "User updated successfully" });
+            return res
+                .status(200)
+                .json({ message: "User updated successfully" });
         }
     });
 };
