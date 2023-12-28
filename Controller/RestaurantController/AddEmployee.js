94051bb74777a7a396cca04fdadd0b4f880a218c
Azz0102 <nguyenduchuyhht2@gmail.com>
2023-12-28 16:57:28 +0700

    huge update

--- Controller/RestaurantController/AddEmployee.js
+++ Controller/RestaurantController/AddEmployee.js
@@ -1,17 +1,20 @@
 const pool = require("../../database");
 
 module.exports.AddEmployee = (req, res) => {
-    const {codeName, resID} = req.body;
-
-    // Construct the SQL query
-    const updateQuery = `UPDATE profile SET RestaurantID = '${resID}' WHERE CodeName = ${codeName}`;
+    const { codeName, resID } = req.body;
 
     // Execute the update query
-    pool.query(updateQuery, (error, result) => {
-        if (error) {
-            return res.status(500).json(error);
-        } else {
-            return res.status(200).json({ message: "Profile updated successfully" });
+    pool.query(
+        "UPDATE profile SET RestaurantID = ? WHERE CodeName = ?",
+        [resID, codeName],
+        (error, result) => {
+            if (error) {
+                return res.status(500).json(error);
+            } else {
+                return res
+                    .status(200)
+                    .json({ message: "Profile updated successfully" });
+            }
         }
-    });
+    );
 };
